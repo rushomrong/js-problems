79fea0cd0d1b3a5b15a43387bf37f5e7c67a9656
rushomrong <rushomarak@gmail.com>
2024-09-07 03:51:55 +0600

    odd numbers push to an array

--- oddaverage.js
+++ oddaverage.js
@@ -3,12 +3,17 @@
  */
 
 function oddAverage(numbers) {
+  //array
+  const odds = [];
   for (const number of numbers) {
     if (number % 2 === 1) {
-      console.log(number);
+      // console.log(number);
+      odds.push(number); //pushing all the odd number in array
     }
   }
+  console.log(odds);
 }
 
 const numbers = [42, 55, 60, 92, 7, 105, 6, 113];
-console.log(numbers);
+const avg = oddAverage(numbers);
+console.log("Average of the odd number is: ", numbers);
