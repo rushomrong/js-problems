b03f23c2429e1ea40742647d8f7a2a2fddbb5576
rushomrong <rushomarak@gmail.com>
2024-09-07 04:03:50 +0600

    updated

--- oddaverage.js
+++ oddaverage.js
@@ -11,9 +11,20 @@ function oddAverage(numbers) {
       odds.push(number); //pushing all the odd number in array
     }
   }
-  console.log(odds);
+  // console.log(odds);
+  // bijor songkha gular jugfoler somosti
+
+  let sum = 0;
+  for (const number of odds) {
+    sum = sum + number;
+  }
+
+  const count = odds.length;
+  console.log(sum, count);
+  const avg = sum / count;
+  return avg;
 }
 
 const numbers = [42, 55, 60, 92, 7, 105, 6, 113];
 const avg = oddAverage(numbers);
-console.log("Average of the odd number is: ", numbers);
+console.log("Average of the odd number is: ", avg);
